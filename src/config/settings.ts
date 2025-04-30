daadc38baa8791b9d0fab0302cdd9e57bc785e8a
alfedan <alfedan@free.fr>
2025-04-30 08:12:31 +0200

    Add files via upload
    global update file

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,7 +1,7 @@
 const settings = {
   city: "Formerie",
-  latitude: 49.66,
-  longitude: 1.82,
+  latitude: 49.5950,
+  longitude: 1.8397,
   timezone: "Europe/Paris",
   openWeatherApiKey: "bd671dfc92f481b98b8fbd7caf4c24cc"
 };
--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -25,14 +25,14 @@ export function Info() {
           {/* Carte du ciel */}
           <section className="bg-gray-900/50 backdrop-blur-sm rounded-lg overflow-hidden">
             <div className="p-6 border-b border-gray-700">
-              <h2 className="text-2xl font-semibold">Carte du Ciel en Direct</h2>
+              <h2 className="text-2xl font-semibold">Carte du Ciel</h2>
               <p className="text-gray-400 mt-2">
-                Visualisez le ciel en temps réel au-dessus de {settings.city}
+                Visualisez le ciel en temps réel.
               </p>
             </div>
             <div className="aspect-[4/3] w-full">
               <iframe
-                src={`https://www.stelvision.com/carte-ciel/visu_carte.php?stelmarq=C&mode_affichage=normal&req=stel&date_j_carte=28&date_m_carte=4&date_a_carte=2025&heure_m=31&heure_h=0&longi=-0.9500000000000&lat=45.9000000000000&lieu=%C3%89chillais&tzone=2&dst_offset=0&taille_carte=848&fond_r=255&fond_v=255&fond_b=255&lang=fr`}
+                src={`https://stellarium-web.org/`}
                 className="w-full h-full border-0"
                 title="Carte du ciel"
                 loading="lazy"
@@ -49,21 +49,15 @@ export function Info() {
               </p>
             </div>
             <div className="relative aspect-[16/9] w-full overflow-hidden">
-              <img src={`https://djlorenz.github.io/astronomy/lp${mapYear}/Europe${mapYear}B.png`}
-                alt={`Carte de pollution lumineuse ${mapYear}`}
-                className="w-full h-full object-cover hover:scale-150 transition-transform duration-300 cursor-move"
+              <iframe
+                src={`https://djlorenz.github.io/astronomy/lp/overlay/dark.html`}
+                className="w-full h-full border-0"
+                title="Carte du ciel"
+                loading="lazy"
               />
             </div>
             <div className="p-4 bg-gray-800/50 text-sm text-gray-400">
-              Source: Light pollution maps by David Lorenz :  
-            <a
-            href="https://djlorenz.github.io/astronomy/lp/overlay/dark.html"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400 hover:underline"
-            >
-             (djlorenz.github.io)
-          </a>
+              Source: Light pollution maps by David Lorenz : djlorenz.github.io
             </div>
           </section>
         </div>
