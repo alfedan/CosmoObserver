f09e397cb8d261d2b233aa9951964cd591c040fe
alfedan <alfedan@free.fr>
2025-05-07 09:30:28 +0200

    Update settings.ts

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,10 +1,10 @@
 const settings = {
-  city: "Formerie",
-  latitude: 49.5950,
-  longitude: 1.8397,
+  city: "XXXXX",
+  latitude: YY.YYYY,
+  longitude: Z.ZZZZ,
   timezone: "Europe/Paris",
-  openWeatherApiKey: "bd671dfc92f481b98b8fbd7caf4c24cc",
-  skycam:"http://alfedan.freeboxos.fr:1984/public.php"
+  openWeatherApiKey: "A1234567890B1234567890C",
+  skycam:"http://Monsite.MonFAI.fr:PortDeRedirection/public.php"
 };
 
 export default settings;
