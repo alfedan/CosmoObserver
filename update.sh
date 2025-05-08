cbd98db895a63d0792edf058c8f8331b12ba79a5
alfedan <alfedan@free.fr>
2025-05-08 11:38:18 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -24,14 +24,6 @@ done
 echo "🔄 Mise à jour du dépôt depuis GitHub..."
 git pull origin main || { echo "❌ Erreur Git"; exit 1; }
 
-echo "📁 Restauration des fichiers de configuration..."
-for file in "${CONFIG_FILES[@]}"; do
-  if [ -f "$BACKUP_DIR/$(basename "$file")" ]; then
-    cp "$BACKUP_DIR/$(basename "$file")" "$file"
-    echo "✅ Restauré : $file"
-  fi
-done
-
 echo "📦 Installation des dépendances..."
 npm install || { echo "❌ npm install a échoué"; exit 1; }
 
@@ -40,21 +32,19 @@ echo "🔍 Version de npm : $(npm -v)"
 echo "🔄 Vérification des vulnérabilitées..."
 npm audit fix || { echo "❌ npm audit a échoué"; exit 1; }
 
-echo "🔧 Correction des vulnérabilités..."
-npm audit fix --force --yes
-AUDIT_STATUS=$?
-
-if [ $AUDIT_STATUS -ne 0 ]; then
-  echo "⚠️ npm audit fix --force a échoué, mais le processus continue..."
-else
-  echo "✅ npm audit fix --force terminé avec succès."
-fi
-
 echo "📦 Installation des dépendances..."
 npm install || { echo "❌ npm install a échoué"; exit 1; }
 
 echo "🔧 Recompilation de l'application..."
 npm run build || { echo "❌ La compilation a échoué"; exit 1; }
+
+echo "📁 Restauration des fichiers de configuration..."
+for file in "${CONFIG_FILES[@]}"; do
+  if [ -f "$BACKUP_DIR/$(basename "$file")" ]; then
+    cp "$BACKUP_DIR/$(basename "$file")" "$file"
+    echo "✅ Restauré : $file"
+  fi
+done
 
 echo "🧹 Nettoyage..."
 rm -r "$BACKUP_DIR"
