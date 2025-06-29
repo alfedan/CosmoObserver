3aef87d05796187ef3b39bb268c2ff696fdf8e44
alfedan <alfedan@free.fr>
2025-06-29 18:40:18 +0200

    Updated settings.ts

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,7 +1,7 @@
 const settings = {
   city: "XXXXX",
-  latitude: YY.YYYY,
-  longitude: Z.ZZZZ,
+  latitude: 12.3456,
+  longitude: 1.2345,
   timezone: "Europe/Paris",
   openWeatherApiKey: "A1234567890B1234567890C",
   skycam:"http://Monsite.MonFAI.fr:PortDeRedirection/public.php"
